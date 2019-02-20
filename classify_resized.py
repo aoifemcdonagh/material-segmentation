e1b964a02274495032ff2d9e79a33c828d73f70e
Aoife <aoifemcdonagh95@hotmail.com>
2019-02-20 10:51:08 +0000

    changing comments

--- classify_resized.py
+++ classify_resized.py
@@ -11,10 +11,12 @@ import numpy as np
 
 
 """
-    TODO: Function needs to be implemented which performs whole image segmentation on an image at three different
-    scales. What should be the output based on next stage of processing? Probability maps for a whole image? seperate
+    TODO: Function needs to be implemented which performs material classification on an image at three different
+    scales. What should be the output based on next stage of processing? Probability maps for a whole image? Separate
     probability maps for each class for each image?
-    possibly give method a name which better reflects its function
+    possibly give method another name which better reflects its function
+
+    This method should call 'classify()' function from full_image_classify
 """
 
 
@@ -22,21 +24,22 @@ import numpy as np
 
 
 """
-
+    Function for resizing and saving an image
+    TODO: decide on paths to save images to wrt function of all other scripts
 """
 
 
 def resize_image(im_path):
-    scale = [1.0/np.sqrt(2), 1.0, np.sqrt(2)]
-    # load input and configure preprocessing
-    im = misc.imread(im_path)
+    scale = [1.0/np.sqrt(2), 1.0, np.sqrt(2)]  # Define scales as per MINC paper
+    im = misc.imread(im_path)  # load image
     im1 = misc.imresize(im, size=scale[0], interp='bilinear')
     im2 = misc.imresize(im, size=scale[1], interp='bilinear')
     im3 = misc.imresize(im, size=scale[2], interp='bilinear')
 
-    dir, file_name = os.path.split(im_path)
-    im_name, ext = os.path.splitext(file_name)
+    dir, file_name = os.path.split(im_path)  # Get directory path and full file name of original image
+    im_name, ext = os.path.splitext(file_name)  # Get file name and extension of original image
 
+    # Use path info from original image to save resized images
     misc.imsave(os.path.join(dir, (im_name + "_1" + ext)), im1)
     misc.imsave(os.path.join(dir, (im_name + "_2" + ext)), im2)
     misc.imsave(os.path.join(dir, (im_name + "_3" + ext)), im3)
