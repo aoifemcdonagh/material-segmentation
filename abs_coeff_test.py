cd0f963fe4cefadd856e9ed76a22363bb006da9a
Aoife <aoifemcdonagh95@hotmail.com>
2019-04-11 12:56:35 +0100

    changing imports, need info on material absorption coefficients to continue developing this script

--- abs_coeff_test.py
+++ abs_coeff_test.py
@@ -2,15 +2,13 @@
 Script for approximating an average absorption coefficient
 """
 
+import caffe
 import sys
-import os
-import skimage
 
-import full_image_classify as minc_utils
-import classify_resized as resize
-from scipy import misc
+import minc_classify as minc_utils
+import segment as resize
 import numpy as np
-from datetime import datetime
+
 
 # Define absorption coefficients for each material
 
@@ -40,6 +38,15 @@ CLASS_LIST = {0: "brick",
 
 abs_coeffs = "/home/gwy-dnn/Documents/Aoife/Reverb/absorption_coeffs.ods"
 
+def get_coefficients(file_path):
+    """
+    Function returning a dict of materials and corresponding absorption coefficient lists
+    :param file_path: path to file containing absorption coefficients
+    :return:
+    """
+
+
+
 def get_average_coeff():
     """
     Function for generating an average absorption coeff based on material classification probabilities
@@ -48,9 +55,10 @@ def get_average_coeff():
 
 
 if __name__ == "__main__":
+    caffe.set_mode_gpu()
     image_path = sys.argv[1]  # path to image to be segmented
 
     output = minc_utils.classify(image_path)
-
-    get_average_coeff()
-    
+    class_map = minc_utils.get_class_map(output)
+    print class_map
+    #get_average_coeff()
