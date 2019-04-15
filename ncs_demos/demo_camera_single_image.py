367c3f43a28eb5a0d80d052aa87c883ba1fe8c99
Aoife <aoifemcdonagh95@hotmail.com>
2019-04-15 17:42:57 +0100

    getting rid of code which is imported from segment

--- ncs_demos/demo_camera_single_image.py
+++ ncs_demos/demo_camera_single_image.py
@@ -7,34 +7,7 @@ import logging as log
 import numpy as np
 from time import time
 import argparse
-import segment as minc_utils
-
-
-CLASS_LIST = {0: "brick",
-              1: "carpet",
-              2: "ceramic",
-              3: "fabric",
-              4: "foliage",
-              5: "food",
-              6: "glass",
-              7: "hair",
-              8: "leather",
-              9: "metal",
-              10: "mirror",
-              11: "other",
-              12: "painted",
-              13: "paper",
-              14: "plastic",
-              15: "polishedstone",
-              16: "skin",
-              17: "sky",
-              18: "stone",
-              19: "tile",
-              20: "wallpaper",
-              21: "water",
-              22: "wood"}
-
-SCALES = [1.0 / np.sqrt(2), 1.0, np.sqrt(2)]  # changed scales
+from segment import get_average_prob_maps
 
 
 def build_argparser():
@@ -54,93 +27,8 @@ def generate_class_map(network_outputs, shape, pad=0):
     :return: upsampled classification map
     """
 
-    av_prob_maps = minc_utils.get_average_prob_maps(network_outputs, shape, pad)
+    av_prob_maps = get_average_prob_maps(network_outputs, shape, pad)
     return av_prob_maps.argmax(axis=0)
-
-
-def get_average_prob_maps(network_outputs, shape, pad=0):
-    """
-    :param shape: shape of original image needed for upsampling
-    :param network_outputs: List of outputs from
-    :return: Probability maps for each class, averaged from resized images probability maps
-    """
-
-    # Get probability maps for each class for each image
-    prob_maps = [get_probability_maps(out) for out in network_outputs]
-
-    # Upsample probability maps to dimensions of original image (plus any padding)
-    upsampled_prob_maps = upsample_prob_maps(prob_maps, output_shape=(shape[1] + pad*2, shape[2] + pad*2))
-
-    # Probability maps for each class, averaged from resized images probability maps
-    averaged_prob_maps = np.average(upsampled_prob_maps, axis=0)
-
-    # Remove the padded sections from the averaged prob maps
-    averaged_prob_maps = [remove_padding(prob_map, pad) for prob_map in averaged_prob_maps]
-
-    return np.array(averaged_prob_maps)
-
-
-def upsample_prob_maps(prob_maps_multiple_images, output_shape):
-    """
-    Function for performing upsamping of probability maps
-    :param prob_maps: Probability maps for each class for each resized image
-    :param output_shape: Desired shape to upsample to (should be dimensions of original image)
-    :return:
-    """
-
-    # Upsampling probability maps to be same dimensions as original image
-    # np.array so that they can be averaged later
-    return np.array([[skimage.transform.resize(prob_map,
-                                               output_shape=output_shape,
-                                               mode='constant',
-                                               cval=0,
-                                               preserve_range=True)
-                      for prob_map in prob_maps_single_image]
-                    for prob_maps_single_image in prob_maps_multiple_images])
-
-
-def resize_image(im):
-    """
-    Resize input image to 3 scales as defined in MINC paper
-    :param image: image with float (16 or 32) values in range [0-255]
-    :return: List of 3 resized images
-    """
-
-    im = im/255.0  # Images of type float must be between -1 and 1 to perform rescaling
-    return [skimage.transform.rescale(im, scale, mode='constant', cval=0)*255.0 for scale in SCALES]
-
-
-def add_padding(im, pad=0):
-    """
-    Function for padding image before classification
-    :param im: image (preloaded with caffe.io.load_image)
-    :param pad: number of pixels of padding to add (default 0)
-    :return: image with padding
-    """
-
-    return np.pad(im, pad_width=((pad, pad), (pad, pad), (0, 0)), mode='symmetric')
-
-
-def remove_padding(im, pad=0):
-    """
-    Function for removing padding from an image
-    :param im: image or prob map to remove padding from
-    :param pad: number of pixels of padding to remove
-    :return:
-    """
-
-    if pad == 0:
-        return im
-    else:
-        return im[pad:-pad, pad:-pad]
-
-
-def get_probability_maps(network_output):
-    """
-    Function which returns all probability maps in a network output
-    Returns a list of probability maps (numpy arrays)
-    """
-    return [network_output['prob'][0][class_num] for class_num in CLASS_LIST.keys()]
 
 
 if __name__ == "__main__":
@@ -208,8 +96,6 @@ if __name__ == "__main__":
         if exec_net.requests[cur_request_id].wait(-1) == 0:
             results = exec_net.requests[cur_request_id].outputs[out_blob]
             class_map = generate_class_map(results, [args.resolution[0], args.resolution[1]], pad=args.padding)
-
-
 
         cv2.imshow("Segmentation results", class_map)
 
