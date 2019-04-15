917f91297f37af5d862e883ddcde2ec6e081f6f6
Aoife <aoifemcdonagh95@hotmail.com>
2019-04-15 18:11:46 +0100

    script replacing segment.py which avoids importing packages which Pi doesn't support

---
+++ ncs_demos/ncs_utilities.py
@@ -0,0 +1,117 @@
+import skimage.transform
+import numpy as np
+
+
+#  Global dictionary containing class number : name pairs
+CLASS_LIST = {0: "brick",
+              1: "carpet",
+              2: "ceramic",
+              3: "fabric",
+              4: "foliage",
+              5: "food",
+              6: "glass",
+              7: "hair",
+              8: "leather",
+              9: "metal",
+              10: "mirror",
+              11: "other",
+              12: "painted",
+              13: "paper",
+              14: "plastic",
+              15: "polishedstone",
+              16: "skin",
+              17: "sky",
+              18: "stone",
+              19: "tile",
+              20: "wallpaper",
+              21: "water",
+              22: "wood"}
+SCALES = [1.0 / np.sqrt(2), 1.0, np.sqrt(2)]  # Define scales as per MINC paper
+
+
+def get_average_prob_maps(network_outputs, shape, pad=0):
+    """
+    :param network_outputs: List of outputs
+    :param shape: shape of original image needed for upsampling
+    :return: Probability maps for each class, averaged from resized images probability maps
+    """
+
+    # Get probability maps for each class for each image
+    prob_maps = [get_probability_maps(out) for out in network_outputs]
+
+    # Upsample probability maps to dimensions of original image (plus any padding)
+    upsampled_prob_maps = upsample(prob_maps, output_shape=(shape[0] + pad*2, shape[1] + pad*2))
+
+    # Probability maps for each class, averaged from resized images probability maps
+    averaged_prob_maps = np.average(upsampled_prob_maps, axis=0)
+
+    # Remove the padded sections from the averaged prob maps
+    averaged_prob_maps = [remove_padding(prob_map, pad) for prob_map in averaged_prob_maps]
+
+    return np.array(averaged_prob_maps)
+
+
+def upsample(prob_maps_multiple_images, output_shape):
+    """
+    Function for performing upsamping of probability maps
+    :param prob_maps: Probability maps for each class for each resized image
+    :param output_shape: Desired shape to upsample to (should be dimensions of original image)
+    :return:
+    """
+
+    # Upsampling probability maps to be same dimensions as original image
+    # np.array so that they can be averaged later
+    return np.array([[skimage.transform.resize(prob_map,
+                                               output_shape=output_shape,
+                                               mode='constant',
+                                               cval=0,
+                                               preserve_range=True)
+                      for prob_map in prob_maps_single_image]
+                    for prob_maps_single_image in prob_maps_multiple_images])
+
+
+def resize_images(im):
+    """
+    Function for resizing an image to scales as defined in MINC paper
+    Not saving images anymore, just returning them for processing
+
+    :param im: pre-loaded image
+    :return: list of resized images
+    """
+
+    return [skimage.transform.rescale(im, scale, mode='constant', cval=0) for scale in SCALES]
+
+
+def get_probability_maps(out):
+    """
+    Function which returns all probability maps in a network output
+    :param out: network output
+    :return: list of probability maps (numpy arrays)
+    """
+
+    return [out['prob'][0][class_num] for class_num in CLASS_LIST.keys()]
+
+
+def add_padding(im, pad=0):
+    """
+    Function for padding image before classification
+    :param im: image (preloaded with caffe.io.load_image)
+    :param pad: number of pixels of padding to add (default 0)
+    :return: image with padding
+    """
+
+    return np.pad(im, pad_width=((pad, pad), (pad, pad), (0, 0)), mode='symmetric')
+
+
+def remove_padding(im, pad=0):
+    """
+    Function for removing padding from an image
+    :param im: image or prob map to remove padding from
+    :param pad: number of pixels of padding to remove
+    :return:
+    """
+
+    if pad == 0:
+        return im
+    else:
+        return im[pad:-pad, pad:-pad]
