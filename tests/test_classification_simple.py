8fff5fa4c28b4bf5669ebd3004db38c99d9dbf06
Aoife <aoifemcdonagh95@hotmail.com>
2019-05-03 18:33:10 +0100

    changing arguments in both scripts to reflect changes in functions in gpu_segment

--- tests/test_classification_simple.py
+++ tests/test_classification_simple.py
@@ -18,13 +18,12 @@ caffe.set_mode_gpu()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", type=str, help="path to image to be classified")
-parser.add_argument("--prototxt", type=str, default="models/deploy-googlenet-conv.prototxt", help="path to prototxt file")
 parser.add_argument("--caffemodel", type=str, default="models/minc-googlenet-conv.caffemodel", help="path to caffemodel file")
 
 args = parser.parse_args()
 
 image = caffe.io.load_image(args.image)  # must load image using caffe.io.load_image(). note this outputs RGB image
-output = classify(image, args.prototxt, args.caffemodel)
+output = classify(image, caffemodel=args.caffemodel)
 
 minc_plot.plot_class_map(output)
 minc_plot.plot_confidence_map(output)
--- tests/test_continuous_upsampling.py
+++ tests/test_continuous_upsampling.py
@@ -1,33 +1,26 @@
-import segment
+# This script performs continuous segmentation with pyramidal upsampling on video/camera stream
+
+from gpu_segment import segment
 import ncs_demos.ncs_utilities as utils
 import cv2
 import logging as log
 import argparse
 import sys
 import os
-import caffe
 
 
 def build_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", help="Path to an .xml file with a trained model.", type=str)
-    parser.add_argument("-i", "--input", help="Input, 'cam' or path to image", required=True,
-                        type=str)
-    parser.add_argument("-u", "--upsample", help="To upsample output", default=False, action="store_true")
+    parser.add_argument("-i", "--input", help="Input, 'cam' or path to image", required=True, type=str)
     parser.add_argument("-p", "--padding", help="Number of pixels of padding to add", type=int, default=0)
-    parser.add_argument("-r", "--resolution", help="desired camera resolution, format [h,w]", nargs="+", type=int)
+    #parser.add_argument("-r", "--resolution", help="desired camera resolution, format [h,w]", nargs="+", type=int)
     return parser
 
 
 if __name__ == "__main__":
-    caffe.set_mode_gpu()
     log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)  # Configure logging
     args = build_argparser().parse_args()
-
-    n = 1
-    c = 3
-    h = args.resolution[0]
-    w = args.resolution[1]
 
     if args.input == 'cam':
         input_stream = 0
@@ -43,7 +36,7 @@ if __name__ == "__main__":
     while cap.isOpened():
         ret, frame = cap.read()
 
-        results = segment.segment(frame, args.padding)
+        results = segment(frame, args.padding, args.model)
         class_map = utils.get_class_map(results)
 
         cv2.imshow('class map', class_map)
