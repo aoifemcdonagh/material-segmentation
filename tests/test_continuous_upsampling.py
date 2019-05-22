ba8f718c9270a2c74d745a663a388dea09a49d41
Aoife <aoife.mcdonagh@xperi.com>
2019-05-21 19:15:47 -0700

    making plots bigger, fixed bug with segmentation; now correctly segmenting RGB image, not BGR image directly from cv2.imread()

--- tests/test_continuous_upsampling.py
+++ tests/test_continuous_upsampling.py
@@ -1,17 +1,17 @@
 # This script performs continuous segmentation with pyramidal upsampling on video/camera stream
 
-from gpu_segment import segment
 import ncs_demos.ncs_utilities as utils
+from bonaire.gpu_segment import segment
 import cv2
 import logging as log
 import argparse
-import sys
 import os
+import sys
 
 
 def build_argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", help="Path to an .xml file with a trained model.", type=str)
+    parser.add_argument("-m", "--model", help="Path to an .xml file of a trained model.", type=str)
     parser.add_argument("-i", "--input", help="Input, 'cam' or path to image", required=True, type=str)
     parser.add_argument("-p", "--padding", help="Number of pixels of padding to add", type=int, default=0)
     #parser.add_argument("-r", "--resolution", help="desired camera resolution, format [h,w]", nargs="+", type=int)
@@ -30,15 +30,17 @@ if __name__ == "__main__":
 
     cap = cv2.VideoCapture(input_stream)
 
-    window_result = cv2.namedWindow("class map", cv2.WINDOW_NORMAL)
-    window_frame = cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+    window_result = cv2.namedWindow("class map")
+    window_frame = cv2.namedWindow("frame")
 
     while cap.isOpened():
         ret, frame = cap.read()
+        # convert current frame to RGB for processing by 'gpu_segment'
+        RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        results = segment(frame, args.padding, args.model)
+        results = segment(RGBframe, args.padding, args.model)
         class_map = utils.get_class_map(results)
 
         cv2.imshow('class map', class_map)
         cv2.imshow('frame', frame)
-        cv2.waitKey(1000)
+        cv2.waitKey(10)
